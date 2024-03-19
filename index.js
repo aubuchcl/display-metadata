d4fcdc1c24cb878e8835af48e75cd146162b91da
Christopher Aubuchon <aubuchcl@gmail.com>
2024-03-19 16:56:01 -0700

    updated

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
-
 const express = require('express');
-const fs = require('fs').promises; // Using fs promises API for simplicity
-const path = require('path');
+const fs = require('fs').promises;
 
-// Initialize the express application
 const app = express();
 
-// Define the /_health endpoint
 app.get('/_health', (req, res) => {
   res.status(200).json({ message: 'OK' });
 });
+
 app.get('/', async (req, res) => {
   try {
     const filePath = '/var/run/cycle/metadata/environment.json';
     const data = await fs.readFile(filePath, 'utf8');
-    res.type('json').send(data); // Set Content-Type as JSON and send data
+    const parsedData = JSON.parse(data);
+    const deploymentsData = parsedData.deployments;
+    const prettyDeploymentsData = JSON.stringify(deploymentsData, null, 2); //
+
+    // Create an HTML content with styling
+    const htmlContent = `
+      <html>
+      <head>
+        <title>JSON Data</title>
+        <style>
+          body {
+            background-color: #333; /* Dark grey background */
+            color: white; /* White text */
+            height: 100vh; /* Full height */
+            margin: 0;
+            display: flex;
+            justify-content: center; /* Center horizontally */
+            align-items: center; /* Center vertically */
+            font-family: Arial, sans-serif;
+          }
+          pre {
+            width: 90%; /* Set a max-width for the pre tag */
+            overflow: auto; /* Enables scrolling if content is too wide */
+          }
+        </style>
+      </head>
+      <body>
+        <pre>${prettyDeploymentsData}</pre>
+      </body>
+      </html>
+    `;
+
+    res.send(htmlContent); // Send the HTML with the JSON data embedded
+
   } catch (error) {
     console.error('Error reading the file:', error);
     res.status(500).send('An error occurred while reading the file.');
   }
 });
 
-// Start the server on port 3000
 const server = app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
-// Gracefully stop the server on SIGINT
 process.on('SIGINT', () => {
   console.log('Received SIGINT, shutting down server...');
   server.close(() => {
